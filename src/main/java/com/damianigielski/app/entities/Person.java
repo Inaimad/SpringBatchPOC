1d202dba0a7beeaf5e4708cfe109e49381e6d191
Damian Igielski <damian.igielski@outlook.com>
2018-12-15 21:18:13 +0100

    Added JPA support and imported H2 DB

--- src/main/java/com/damianigielski/app/entities/Person.java
+++ src/main/java/com/damianigielski/app/entities/Person.java
@@ -1,9 +1,18 @@
 package com.damianigielski.app.entities;
 
+import javax.persistence.*;
+
+@Entity
 public class Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "first_name")
     private String firstName;
 
 
@@ -13,6 +22,14 @@ public class Person {
     public Person(String lastName, String firstName) {
         this.lastName = lastName;
         this.firstName = firstName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getLastName() {
--- target/classes/schema-all.sql
+++ target/classes/schema-all.sql
@@ -6,10 +6,10 @@ CREATE TABLE people  (
     last_name VARCHAR(20)
 );
 
-INSERT INTO people (person_id, first_name, last_name) VALUES (1, 'Jan', 'Nowak');
-INSERT INTO people (person_id, first_name, last_name) VALUES (2, 'Adam', 'Kowalski');
-INSERT INTO people (person_id, first_name, last_name) VALUES (3, 'Tomasz', 'Korniszon');
-INSERT INTO people (person_id, first_name, last_name) VALUES (4, 'Wojciech', 'Wsadowy');
+INSERT INTO people (first_name, last_name) VALUES ('Jan', 'Nowak');
+INSERT INTO people (first_name, last_name) VALUES ('Adam', 'Kowalski');
+INSERT INTO people (first_name, last_name) VALUES ('Tomasz', 'Korniszon');
+INSERT INTO people (first_name, last_name) VALUES ('Wojciech', 'Wsadowy');
 
 DROP TABLE converted_people IF EXISTS;
 
